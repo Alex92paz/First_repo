4ccfab841e0267a58800e6d670be24869520b83d
Alex92paz <alex1992paz@gmail.com>
2024-06-14 09:38:38 +0300

    Version 0

---
+++ test23.py
@@ -0,0 +1,8 @@
+my_list = [2024, 3.12]
+some_data = ['Python']
+
+my_list.extend(some_data)   # Додає ['Python'] до кінця my_list
+my_list.insert(1, 'Python') # Вставляє 'Python' на позицію 1
+my_list.reverse()           # Реверсує список
+
+print(my_list)              # Виводить оновлений список
