b83f8a50337fa9ec576eaae83b4c1b21a4d2c7aa
Alex92paz <alex1992paz@gmail.com>
2024-10-06 19:55:18 +0300

    Version 2

--- Help.py
+++ Help.py
@@ -1,8 +1,29 @@
-FLOORS = 5
-APARTMENTS_PER_FLOOR = 4
+# Создание пустого словаря
+cat = {}
 
-apartment_number = int(input('Enter apartment number: '))
-apartments_per_entrance = FLOORS * APARTMENTS_PER_FLOOR
-entrance_number = (apartment_number - 1) // apartments_per_entrance + 1
-floor_number = ((apartment_number - 1) % apartments_per_entrance) // APARTMENTS_PER_FLOOR + 1
-print(f"Entrance number {entrance_number}, Floor number {floor_number}")
+# Добавление ключа "nick"
+cat['nick'] = 'Simon'
+print(cat)
+
+# Добавление ключа "age"
+cat['age'] = 7
+print(cat)
+
+# Добавление ключа "characteristics"
+cat['characteristics'] = ["лагідний", "кусається"]
+print(cat)
+
+# Объявление словаря с дополнительной информацией
+info = {"status": "vaccinated", "breed": True}
+
+
+# Использование метода get для получения возраста кота
+age = cat.get('age')
+
+
+# Использование метода update для добавления всех пар ключ-значение из словаря info
+cat.update(info)
+print(cat)
+
+# Вывод словаря cat
+print(cat)
